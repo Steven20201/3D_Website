96f1e55fbf3f9ee1110a93ea6a54b25ccb7f3b88
Steven20201 <khantzawhein261@gmail.com>
2024-01-05 14:37:57 +0630

    end

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
    <nav
-    className = {`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    className = {`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-black`}
    >
     <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
       <Link
@@ -22,7 +22,7 @@ const Navbar = () => {
         }}
       >
         <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-        <p className='text-white text-[18] font-bold cursor-pointer flex'>Steve &nbsp;<span className='sm:block hidden'>| JS Mastery</span></p>
+        <p className='text-white text-[18] font-bold cursor-pointer flex'>Steve Winchester</p>
       </Link>
       <ul className='list-none hidden sm:flex flex-row gap-10'>
         {navLinks.map((link) => (
